e35581342a663d8f0a6621a2c264f05ba26676e6
Kathryn Browning <kbrowni3@my.westga.edu>
2015-02-03 18:40:50 -0500

    Added methods to CustomerList.cs

--- App_Code/CustomerList.cs
+++ App_Code/CustomerList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Activities.Debugger;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,48 @@ using System.Web;
 /// </summary>
 public class CustomerList
 {
+    private List<Customer> _customerList;
+ 
 	public CustomerList()
 	{
 		//
 		// TODO: Add constructor logic here
 		//
-	}
+    }
+
+    public Customer this[int index]
+    {
+      
+    }
+
+    public Customer this[String name]
+    {
+
+    }
+
+    public int Count()
+    {
+        
+    }
+
+    public void AddItem(Customer newCustomer)
+    {
+        
+    }
+
+    public void RemoveAt(int index)
+    {
+       
+    }
+
+    public void Clear()
+    {
+        
+    }
+
+    public static CustomerList GetCustomers()
+    {
+        
+    }
+
 }
