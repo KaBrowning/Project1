076807cf49cbe624056690a710e398fb55648af4
Kathryn Browning <kbrowni3@my.westga.edu>
2015-02-04 23:01:18 -0500

    Started the code behind to get all of the data in the Feedback table

--- FeedbackComplete.aspx.cs
+++ FeedbackComplete.aspx.cs
@@ -7,8 +7,20 @@ using System.Web.UI.WebControls;
 
 public partial class FeedbackComplete : System.Web.UI.Page
 {
+
+    private string _feedbackId;
+    private string _customerId;
+    private string _softwareId;
+    private string _supportedId;
+    private string _dateOpened;
+    private string _dateClosed;
+    private string _title;
+    private string _desription;
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
+
+
 }
